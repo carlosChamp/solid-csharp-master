e42f22036ce033193921a547c587638a6e22202c
Carlos André <carlos_andre.1@hotmail.com>
2023-09-06 09:52:47 -0300

    :test_tube: test: Mockando testes

--- tests/Alura.LeilaoOnline.Testes/LeilaoControllerRemove.cs
+++ tests/Alura.LeilaoOnline.Testes/LeilaoControllerRemove.cs
@@ -2,18 +2,62 @@ using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using Alura.LeilaoOnline.WebApp.Controllers;
+using Xunit.Sdk;
+using System.Reflection;
+using System.Collections.Generic;
+using Alura.LeilaoOnline.WebApp.Models;
 
 namespace Alura.LeilaoOnline.Testes
 {
     public class LeilaoControllerRemove
     {
+        MockUpData.MockAdminService mock = new MockUpData.MockAdminService();
+
+        public LeilaoControllerRemove()
+        {
+            Leilao LeilaoEmPregao = new Leilao()
+            {
+                Id = 1,
+                Categoria = new Categoria()
+                {
+                    Descricao = "Teste",
+                    Id = 1,
+                    Imagem = ""
+                },
+                Descricao = "Teste Leilão",
+                IdCategoria = 1,
+                Inicio = DateTime.Now,
+                Situacao = SituacaoLeilao.Pregao,
+                Titulo = "Teste 1 - Leilão teste 1"
+            };
+            Leilao LeilaoEmRascunho = new Leilao()
+            {
+                Id = 2,
+                Categoria = new Categoria()
+                {
+                    Descricao = "Teste",
+                    Id = 1,
+                    Imagem = ""
+                },
+                Descricao = "Teste Leilão 2",
+                IdCategoria = 1,
+                Inicio = DateTime.Now,
+                Situacao = SituacaoLeilao.Rascunho,
+                Titulo = "Teste 1 - Leilão teste 1"
+            };
+
+
+            mock.CadastraLeilao(LeilaoEmPregao);
+            mock.CadastraLeilao(LeilaoEmRascunho);
+        }
+
         [Fact]
         public void DadoLeilaoInexistenteEntaoRetorna404()
         {
             // arrange
             var idLeilaoInexistente = 11232; // preciso entrar no banco para saber qual é inexistente!! teste deixa de ser automático...
             var actionResultEsperado = typeof(NotFoundResult);
-            var controller = new LeilaoController();
+            var controller = new LeilaoController(mock);
 
             // act
             var result = controller.Remove(idLeilaoInexistente);
@@ -26,9 +70,9 @@ namespace Alura.LeilaoOnline.Testes
         public void DadoLeilaoEmPregaoEntaoRetorna405()
         {
             // arrange
-            var idLeilaoEmPregao = 11232; // qual leilao está em pregão???!! 
+            var idLeilaoEmPregao = 1; // qual leilao está em pregão???!! 
             var actionResultEsperado = typeof(StatusCodeResult);
-            var controller = new LeilaoController();
+            var controller = new LeilaoController(mock);
 
             // act
             var result = controller.Remove(idLeilaoEmPregao);
@@ -41,9 +85,9 @@ namespace Alura.LeilaoOnline.Testes
         public void DadoLeilaoEmRascunhoEntaoExcluiORegistro()
         {
             // arrange
-            var idLeilaoEmRascunho = 11232; // qual leilao está em rascunho???!!
+            var idLeilaoEmRascunho = 2; // qual leilao está em rascunho???!!
             var actionResultEsperado = typeof(NoContentResult);
-            var controller = new LeilaoController();
+            var controller = new LeilaoController(mock);
 
             // act
             var result = controller.Remove(idLeilaoEmRascunho);
@@ -51,5 +95,8 @@ namespace Alura.LeilaoOnline.Testes
             // assert
             Assert.IsType(actionResultEsperado, result);
         }
+
     }
+
+    
 }
---
+++ tests/Alura.LeilaoOnline.Testes/MockUpData/MockAdminService.cs
@@ -0,0 +1,80 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Alura.LeilaoOnline.WebApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Testes.MockUpData
+{
+    internal class MockAdminService : IAdminService
+    {
+        public List<Leilao> ListaLeiloes { get; set; } = new List<Leilao>();    
+
+        public MockAdminService() 
+        {
+
+        }
+        public void CadastraLeilao(Leilao leilao)
+        {
+            ListaLeiloes.Add(leilao);
+        }
+
+
+        public IEnumerable<Categoria> ConsultaCategorias()
+        {
+            return new List<Categoria>(){
+                new Categoria()
+                    {
+                        Descricao = "Teste",
+                        Id = 1,
+                        Imagem = ""
+                    },
+
+            };
+        }
+
+        public Leilao ConsultaLeilaoPorId(int id)
+        {
+            return new Leilao()
+            {
+                Id = id,
+                Categoria = new Categoria()
+                {
+                    Descricao = "Teste",
+                    Id = 1,
+                    Imagem = ""
+                },
+                Descricao = "Teste Leilão",
+                IdCategoria = 1,
+                Inicio = DateTime.Now,
+                Situacao = SituacaoLeilao.Pregao,
+                Titulo = "Teste 1 - Leilão teste 1"
+            };
+        }
+
+        public IEnumerable<Leilao> ConsultaLeiloes()
+        {
+            return ListaLeiloes;
+        }
+
+        public void FinalizaPregaoDoLeilaoComId(int id)
+        {
+            
+        }
+
+        public void IniciaPregaoDoLeilaoComId(int id)
+        {
+            
+        }
+
+        public void ModificaLeilao(Leilao leilao)
+        {
+            
+        }
+
+        public void RemoveLeilao(Leilao leilao)
+        {
+            
+        }
+    }
+}
